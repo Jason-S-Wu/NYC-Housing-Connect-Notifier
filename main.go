251323f1e90284266147ad47eea05dec83ec3275
Jason Wu <jason@jswu.me>
2025-08-08 03:58:35 -0400

    Update .env.sample and main.go to include SLEEP_DURATION_HOURS variable and handle its default value

--- main.go
+++ main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"log"
-	"os"
-	"strconv"
-
 	"main/models"
 	"main/utils/discord"
 	"main/utils/fetch_housing_connect"
 	"main/utils/local"
+	"os"
+	"strconv"
+	"time"
 
 	"github.com/joho/godotenv"
 )
@@ -20,6 +20,7 @@ func main() {
 	householdIncomeStr := os.Getenv("HOUSEHOLD_INCOME")
 	householdSizeStr := os.Getenv("HOUSEHOLD_SIZE")
 	saveFileName := os.Getenv("SAVE_FILE_NAME")
+	sleepDurationHrsStr := os.Getenv("SLEEP_DURATION_HOURS")
 
 	householdIncome, err := strconv.Atoi(householdIncomeStr)
 	if err != nil {
@@ -30,24 +31,34 @@ func main() {
 		log.Fatalf("Invalid HOUSEHOLD_SIZE: %v", err)
 	}
 
-	rentalData, err := fetch_housing_connect.FetchAllLotteries(householdIncome, householdSize)
+	sleepDurationHrs, err := strconv.Atoi(sleepDurationHrsStr)
 	if err != nil {
-		log.Fatalf("Error fetching lotteries: %v", err)
+		sleepDurationHrs = 6 // Default to 6 hours if parsing fails
 	}
 
-	oldRentals, err := local.ReadRentalsFromFile(saveFileName)
-	if err != nil {
-		log.Printf("Error reading old rentals from file: %v", err)
-		oldRentals = []models.Rental{} // If file read fails, start with an empty slice
+	for {
+		rentalData, err := fetch_housing_connect.FetchAllLotteries(householdIncome, householdSize)
+		if err != nil {
+			log.Printf("Error fetching lotteries: %v", err)
+		} else {
+			oldRentals, err := local.ReadRentalsFromFile(saveFileName)
+			if err != nil {
+				log.Printf("Error reading old rentals from file: %v", err)
+				oldRentals = []models.Rental{}
+			}
+
+			newRentals := local.NewRentals(oldRentals, rentalData)
+
+			println("New Rentals:")
+			for _, rental := range newRentals {
+				println(rental.String())
+				discord.SendRentalNotification(webhookURL, rental)
+			}
+
+			local.WriteRentalsToFile(rentalData, saveFileName)
+		}
+
+		log.Printf("Sleeping for %d hours...\n", sleepDurationHrs)
+		time.Sleep(time.Duration(sleepDurationHrs) * time.Hour)
 	}
-
-	newRentals := local.NewRentals(oldRentals, rentalData)
-
-	println("New Rentals:")
-	for _, rental := range newRentals {
-		println(rental.String())
-		discord.SendRentalNotification(webhookURL, rental)
-	}
-
-	local.WriteRentalsToFile(rentalData, saveFileName)
 }
