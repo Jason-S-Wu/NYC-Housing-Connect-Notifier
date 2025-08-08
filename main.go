d3acb35a541bc75605c15ca81021a2a8b690f1e4
Jason Wu <jason@jswu.me>
2025-08-08 03:48:35 -0400

    fixed .env

--- main.go
+++ main.go
@@ -14,11 +14,7 @@ import (
 )
 
 func main() {
-	// Load .env file
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	godotenv.Load()
 
 	webhookURL := os.Getenv("DISCORD_WEBHOOK")
 	householdIncomeStr := os.Getenv("HOUSEHOLD_INCOME")
